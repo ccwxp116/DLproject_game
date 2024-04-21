2f82ded5fa655b83a968a41d2ad3db40466e8d1c
小皮Cici Wang <cici.wsq@gmail.com>
2024-04-21 14:25:45 -0400

    filtered 5 col and randomly select 2000 data points, added in filted data

--- test_file.py
+++ test_file.py
@@ -1,1 +1,20 @@
 import pandas as pd
+
+data = pd.read_csv("/Users/ciciwxp/Desktop/dlproj_data/games.csv")
+
+print(data.shape)
+
+print(data.columns)
+
+selected_columns = ['About the game', 'Reviews', 'Header image', 'Screenshots', 'Genres']
+
+missing_columns = [col for col in selected_columns if col not in data.columns]
+if not missing_columns:
+    sampled_data = data[selected_columns].sample(n=2000, random_state=42)
+else:
+    sampled_data = None
+
+(sampled_data.head() if sampled_data is not None else "Missing Columns: " + ", ".join(missing_columns))
+
+sampled_data.shape 
+sampled_data.to_csv('/Users/ciciwxp/Desktop/CSCI2470/DLproject_game/data.csv', index=False)
